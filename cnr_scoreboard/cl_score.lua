ec4aa2c22fb63ab183236cf8cfceae1523adb3b3
rhapidfyre <maharris84@hotmail.com>
2019-08-25 20:16:36 -0700

    Scoreboard
    Fixed the scoreboard. Names now show up white blue or orange respectively. Also made the floating name tags above the head work properly.

--- cnr_scoreboard/cl_score.lua
+++ cnr_scoreboard/cl_score.lua
@@ -25,7 +25,7 @@ local copColors = {
 }
 
 local wantedColors = {
-  [1]  = {255,225,125},  [2]  = {255,200,100},
+  [1]  = {255,225,125}, [2]  = {255,200,100},
   [3]  = {255,190,86},  [4]  = {255,146,70},
   [5]  = {255,130,60},  [6]  = {255,122,45},
   [7]  = {255,112,32},  [8]  = {255,105,25},
@@ -66,21 +66,11 @@ Citizen.CreateThread(function()
 				for _,i in ipairs(plys) do
 					local uname = GetPlayerName(i)
 					local svid  = GetPlayerServerId(i)
-          
+          print("wantedPlayers["..svid.."] = "..tostring(wantedPlayers[svid]))
           if wantedPlayers[svid] then
             print("DEBUG - has WP value.")
-            -- Is a cop
-            if wantedPlayers[svid] < 0 then
-              print("DEBUG - is a cop.")
-              table.insert(players, '<div class="ply_info">'..
-                '<h3>'..(uname)..'</h3><h5>'..(svid)..'</h5><table>'..
-                '<tr><thead><th colspan="2">Law Enforcement</th></thead></tr>'..
-                '<tr><th>Cop Level</th><td>1</td></tr>'..
-                '<tr><th>Civ Level</th><td>1</td></tr>'..
-                '</table></div>'
-              )
             -- Is Wanted
-            elseif wantedPlayers[svid] > 0 then
+            if wantedPlayers[svid] > 0 then
               print("DEBUG - is wanted.")
               if wantedPlayers[svid] > 10 then -- Most Wanted
                 print("DEBUG - is most wanted.")
@@ -115,6 +105,21 @@ Citizen.CreateThread(function()
                 '</table></div>'
               )
             end
+          elseif copPlayers[svid] then
+            -- Is a cop
+            local cLev = math.floor(copPlayers[svid]/10)
+            if cLev <= 0 then cLev = 1
+            elseif cLev > 10 then cLev = 10 end
+            print("DEBUG - is a cop.")
+            table.insert(players, '<div class="ply_info">'..
+              '<h3>'..(uname)..'</h3><h5>'..(svid)..'</h5>'..
+              '<table class="police'..(cLev)..'">'..
+              '<tr><thead><th colspan="2">Law Enforcement</th></thead></tr>'..
+              '<tr><th>Cop Level</th><td>1</td></tr>'..
+              '<tr><th>Civ Level</th><td>1</td></tr>'..
+              '</table></div>'
+            )
+            
           -- Is Not Wanted
           else
             print("DEBUG - no WP value found; Not wanted.")
@@ -169,26 +174,32 @@ end)
 
 
 -- Draws the text above the head
-function DrawText3D(x,y,z, text, scale, col) -- some useful function, use it if you want!
-  local onScreen,_x,_y = World3dToScreen2d(x,y,z)
-  if not scale then
-    scale = {[1] = 0.4, [2] = 0.4}
-  end 
+function DrawText3D(x,y,z, text, col) -- some useful function, use it if you want!
+  local onScreen,_x,_y = GetScreenCoordFromWorldCoord(x,y,z)
+  local dist = GetDistanceBetweenCoords(GetGameplayCamCoords(), x, y, z, 1)
+  local scale = (4.00001 / dist) * 0.3
+  if     scale >  0.2 then scale = 0.20
+  elseif scale < 0.15 then scale = 0.15
+  end
+  
+  local fov = (1/GetGameplayCamFov()) * 100
   if onScreen then
     --SetTextScale(0.0*scale, 0.55*scale)
-    SetTextScale(scale[1], scale[2])
-    SetTextFont(0)
-    SetTextProportional(1)
+    SetTextScale(scale + 0.12, scale + 0.12)
+    SetTextFont(4)
+    SetTextProportional(true)
     -- SetTextScale(0.0, 0.55)
     SetTextColour(col[1], col[2], col[3], 255)
     SetTextDropshadow(0, 0, 0, 0, 255)
     SetTextEdge(2, 0, 0, 0, 150)
+    SetTextCentre(true)
     SetTextDropShadow()
     SetTextOutline()
+    
     SetTextEntry("STRING")
-    SetTextCentre(1)
     AddTextComponentString(text)
-    DrawText(_x,_y)
+    DrawText(_x,_y - 0.025)
+    
   end
 end
 
@@ -201,17 +212,10 @@ function NameColoring(sv)
     end
   end
   
-  if copPlayers[sv] then 
-    local wl = (math.floor(copPlayers[sv]/12))
-    if copPlayers[sv] > 0 and copPlayers[sv] < 101 then 
-      if wl <= 9 then 
-        return copColors[1]
-      else 
-        return copColors[wl]
-      end
-    elseif copPlayers[sv] > 100 then
-      return copColors[11]
-    end
+  local cplayer = copPlayers[sv]
+  if cplayer then
+    local ccolor = copColors[cplayer]
+    if ccolor then return ccolor end
   end
   
   return {255,255,255}
@@ -235,26 +239,11 @@ Citizen.CreateThread(function()
       
         if HasEntityClearLosToEntity(PlayerPedId(), ped, 17) then
           if (ignorePlayerNameDistance) then
-            DrawText3D(x2, y2, z2 + 1.2,
-              (sv),
-              {0.185,0.185}, nameColor
-            )
+            DrawText3D(x2, y2, z2+1, sv, nameColor)
           else
             if ((distance < disIdentifier)) then
-              local a = 1 - distance/128
-              local b = distance/128
-              if b < 0.2 then b = 0.2 end
-              if IsPedInAnyVehicle(ped) then 
-                DrawText3D(x2, y2, z2 + (b * 5.2),
-                  (sv)..'\n'..GetPlayerName(i),
-                  {a*0.25,a*0.25}, nameColor
-                )
-              else
-                DrawText3D(x2, y2, z2 + 1.2,
-                  (sv)..'\n'..GetPlayerName(i),
-                  {a*0.25,a*0.25}, nameColor
-                )
-              end
+              local idName = sv
+              DrawText3D(x2, y2, z2+1, GetPlayerName(i).." ["..sv.."]", nameColor)
             end
           end
         end
@@ -270,37 +259,24 @@ AddEventHandler('cnr:client_unload', function()
 end)
 
 
---- EVENT: 'cl_wanted_player'
--- Updates a single entry for a single player.
--- Triggers 'is_wanted' (if 0 -> X) and 'is_clear' (X -> 0) events respectively
--- Also triggers 'is_most_wanted' if wp exceeds 100
--- @param ply The server ID
--- @param wps The wanted points value
-RegisterNetEvent('cnr:cl_wanted_client')
-AddEventHandler('cnr:cl_wanted_client', function(ply, wp)
-  if wp <= 0.0 then
-    wantedPlayers[ply] = 0
-  elseif wp > 100 then
-    wantedPlayers[ply] = 11
-  else
-    wantedPlayers[ply] = math.floor(wp / 10) + 1
+--- EVENT: 'wanted_client'
+-- Updates the client's entire table with the current server wanted list
+-- @param wanteds The list (table) of wanted players (K: Server ID, V: Points)
+RegisterNetEvent('cnr:wanted_client')
+AddEventHandler('cnr:wanted_client', function(ply, wp)
+  -- If ply not given, return 0
+  if not ply      then return 0 end
+  if not wp       then wantedPlayers[ply] =  0 end -- Create entry if not exists
+  if     wp <   1 then wantedPlayers[ply] =  0
+  elseif wp > 100 then wantedPlayers[ply] = 11
+  else wantedPlayers[ply] = (math.floor((wp/10)) + 1)
   end
-  print("DEBUG - New wanted level ("..wantedPlayers[ply]..") for ply ("..ply..")")
 end)
 
 
---- EVENT: 'cl_wanted_list'
--- Updates the client's entire table with the current server wanted list
--- @param wanteds The list (table) of wanted players (K: Server ID, V: Points)
-RegisterNetEvent('cnr:cl_wanted_list')
-AddEventHandler('cnr:cl_wanted_list', function(wanteds)
-  wantedPlayers = wanteds
-  for k,v in pairs(wantedPlayers) do 
-    local temp = v
-    if v < 1 then       wantedPlayers[ply] = 0
-    elseif v > 100 then wantedPlayers[ply] = 11
-    else wantedPlayers[ply] = (math.floor((wantedPlayers[ply])/10) + 1)
-    end
-  end
+RegisterNetEvent('cnr:police_officer_duty')
+AddEventHandler('cnr:police_officer_duty', function(ply, isDuty, cLevel)
+  if isDuty then copPlayers[ply] = cLevel
+  else copPlayers[ply] = nil end
 end)
 
