11fe103784679966de9647179c89bf2e9f227c7a
rhapidfyre <maharris84@hotmail.com>
2019-12-12 15:11:44 -0800

    New login is ready

--- cnr_chat/sh_chat.lua
+++ cnr_chat/sh_chat.lua
@@ -4,8 +4,8 @@
 urls = {
 
   -- Game Status Feed
-  feed = "https://discordapp.com/api/webhooks/614207378791071744/ZG1quo6TI-WiJwDEKwXDvyMA0mZAgEtlAb9_ruM8l5tqS_IJO6ZAgBi8wSv8SokHTkL0",
+  feed = "https://discordapp.com/api/webhooks/634831765344616485/ONwDIsdKDhQs5Lmij14RACCtL-5ayHjxKrMeKc3BsGHTjTc9rGH_wFc_0I469IbwI8lE",
   
   -- Basically emergencies and 911 calls
-  emg  = "https://discordapp.com/api/webhooks/614209606511493147/RanDk3hsVsi39FrUuldoCxZtF4qAvy6BTPGB3dbJfMZXTTwoelWahTIJzbFIetKUlorN"
+  emg  = "https://discordapp.com/api/webhooks/634831765344616485/ONwDIsdKDhQs5Lmij14RACCtL-5ayHjxKrMeKc3BsGHTjTc9rGH_wFc_0I469IbwI8lE"
 }
