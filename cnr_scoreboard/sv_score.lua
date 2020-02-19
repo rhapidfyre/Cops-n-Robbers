487d68766b68be78745623615404c4ab7ad71944
rhapidfyre <maharris84@hotmail.com>
2020-02-19 13:08:53 -0800

    Added point values

--- cnr_scoreboard/sv_score.lua
+++ cnr_scoreboard/sv_score.lua
@@ -2,15 +2,16 @@
 local cprint = function(msg) exports['cnrobbers']:ConsolePrint(msg) end
 
 local points = {
-  'busted' = 10,
+  bust   = 9,  kill   = 6, kcop   = 9, burg = 3,
+  steal  = 2,  rob    = 4, fish   = 1, hunt = 1,
+  rape   = 5,  craft  = 1, escape = 3
 }
 
 -- Adjust player's score accordingly
 AddEventHandler('cnr:imprisoned', function(ply, cop, wLevel)
   
   local uid = exports['cnrobbers']:UniqueId(cop)
-  
-  local pts = wLevel * points.busted
+  local pts = wLevel * points.bust
   
   -- Add pts to player's cop score
   exports['ghmattimysql']:execute(
