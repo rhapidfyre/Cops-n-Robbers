092ef17fd7d20df02a1207df72d180c67bf3557c
rhapidfyre <maharris84@hotmail.com>
2020-02-24 14:01:07 -0800

    Added baseevents dependency for the time being

--- cnr_job_illegal/__resource.lua
+++ cnr_job_illegal/__resource.lua
@@ -12,7 +12,7 @@
 resource_manifest_version  '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 ui_page "nui/ui.html"
-dependency 'cnrobbers'
+dependencies {'baseevents', 'cnrobbers'}
 
 files {
 	"nui/ui.html",
