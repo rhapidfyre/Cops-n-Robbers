2acab2c933fb7a7f4407ea407d55fdf2d6eda900
rhapidfyre <maharris84@hotmail.com>
2020-02-26 13:01:00 -0800

    Initial file setup for 24/7 stores

---
+++ cnr_stores/__resource.lua
@@ -0,0 +1,65 @@
+
+--[[
+  Cops and Robbers: Inventory Resource
+  Created by Michael Harris ( mike@harrisonline.us )
+  02/24/2020
+
+  This file contains all inventory related information as well as
+  interfaces. Adding, removing, manipulating items as well as 24/7 purchasing
+--]]
+
+
+resource_manifest_version  '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+ui_page "nui/ui.html"
+dependencies  {'cnrobbers','ghmattimysql'}
+
+files {
+	"nui/ui.html",  "nui/ui.js",  "nui/ui.css",
+  "nui/stars/1.png",  "nui/stars/2.png",
+  "nui/stars/3.png",  "nui/stars/4.png",
+  "nui/stars/5.png",  "nui/stars/6.png",
+  "nui/stars/7.png",  "nui/stars/8.png",
+  "nui/stars/9.png",  "nui/stars/10.png",
+  "nui/stars/11.png", "nui/stars/a.png",
+  "nui/stars/b.png",  "nui/stars/c.png",
+  "nui/crimefree.png"
+}
+
+client_scripts {
+  "sh_config.lua",
+  "cl_config.lua",
+  "sh_inventory.lua",
+  "cl_inventory.lua"
+}
+
+server_scripts {
+  "sh_config.lua",
+  "sv_config.lua",
+  "sh_inventory.lua",
+  "sv_inventory.lua"
+}
+
+server_exports {
+  "UpdateInventory",      -- Called anytime a script modifies the inventory
+  "ItemAdd",              -- See notes below
+  "ItemRemove",           -- See notes below
+  "GetInventory",
+  "GetWeight"
+}
+
+exports {
+  "GetInventory",
+  "GetWeight"
+}
+
+--[[
+
+  NOTES
+    itemInfo:
+      ['id']      = Database ID # of the item (not an option for 'ADD')
+      ['name']    = The game name of the item ('drink_beer')
+      ['title']   = The proper name of them item ('Beer')
+      ['consume'] = True if useable, false if not
+      
+]]
---
+++ cnr_stores/sh_config.lua
@@ -0,0 +1,264 @@
+
+--[[
+  Cops and Robbers: Wanted Script - Shared Dependencies
+  Created by Michael Harris (mike@harrisonline.us)
+  08/20/2019
+
+  This file's main purpose is the definition of criminal events.
+
+  We don't want client modders to modify the fines or times they would receive.
+  Thus, the variable is secure to this file and only accessible by accessors.
+
+--]]
+
+
+mw     = 101 -- The value a player becomes "Most Wanted"
+felony = 40  -- The value a player becomes a Felon.
+wanted = {} -- Table of wanted players (KEY: Server Id, VAL: Points)
+
+
+-- List of emergency vehicles by MODELNAME
+eVehicle = {
+  "POLICE", "POLICE2", "POLICE3", "POLICE4",
+  "SHERIFF", "SHERIFF2",
+  "FBI", "FBI2", "FIB", "FIB2",
+  "PRANGER", "FBIRANCHER",
+  "HYDRA", "RHINO", "BARRACKS"
+}
+
+
+--[[
+  VAR: crimes
+  INFO: Holds the crimes information.
+  KEY: crime designation/event
+  VALUE: (Table)
+    title:    The title of the crime for display purposes/referencing
+    weight:   Used for organizing seriousness of a crime (1 being most serious)
+    minTime:  The minimum time that can be given for this crime
+    maxTime:  The maximum time that can be given for this crime
+    isFelony: This crime can make the player exceed Wanted Level 5
+    fine:     The amount of the fine (if applicable)
+]]
+local crimes = {
+  ['gta-npc'] = {
+    title = "Grand Theft Auto",
+    weight = 51, minTime = 0, maxTime = 0, isFelony = false,
+    fine = function() return (math.random(10, 50)) end
+  },
+  ['gta'] = {
+    title = "Grand Theft Auto",
+    weight = 76, minTime = 0, maxTime = 0, isFelony = true,
+    fine = function() return (math.random(10, 50)) end
+  },
+  ['carjack-npc'] = {
+    title = "Carjacking",
+    weight = 31, minTime = 0, maxTime = 0, isFelony = true,
+    fine = function() return (math.random(100, 250)) end
+  },
+  ['664-carjack'] = {
+    title = "Attempted Carjacking",
+    weight = 9, minTime = 2, maxTime = 12, isFelony = false,
+    fine = function() return (math.random(25, 125)) end
+  },
+  ['carjack'] = {
+    title = "Carjacking",
+    weight = 61, minTime = 10, maxTime = 25, isFelony = true,
+    fine = function() return (math.random(100, 400)) end
+  },
+  ['murder'] = {
+    title = "Murder",
+    weight = 190, minTime = 90, maxTime = 120, isFelony = true,
+    fine = function() return (math.random(5000, 8000)) end
+  },
+  ['murder-leo'] = {
+    title = "Murder of a LEO",
+    weight = 220, minTime = 10, maxTime = 25, isFelony = true,
+    fine = function() return (math.random(6250, 12000)) end
+  },
+  ['manslaughter'] = {
+    title = "Manslaughter",
+    weight = 45, minTime = 5, maxTime = 10, isFelony = false,
+    fine = function() return (math.random(1000, 2000)) end
+  },
+  ['adw'] = {
+    title = "Assault with a Deadly Weapon",
+    weight = 60, minTime = 5, maxTime = 20, isFelony = true,
+    fine = function() return (math.random(500, 800)) end
+  },
+  ['assault'] = {
+    title = "Simple Assault",
+    weight = 10, minTime = 1, maxTime = 5, isFelony = false,
+    fine = function() return (math.random(50, 100)) end
+  },
+  ['discharge'] = {
+    title = "Firearm Discharge",
+    weight = 12, minTime = 1, maxTime = 5, isFelony = false,
+    fine = function() return (math.random(20, 40)) end
+  },
+  ['vandalism'] = {
+    title = "Vandalism",
+    weight = 5, minTime = 1, maxTime = 5, isFelony = false,
+    fine = function() return (math.random(10, 20)) end
+  },
+  ['robbery'] = {
+    title = "Armed Robbery",
+    weight = 90, minTime = 20, maxTime = 30, isFelony = true,
+    fine = function() return (math.random(500, 2000)) end
+  },
+  ['robbery-sa'] = {
+    title = "Strong-Arm Robbery",
+    weight = 42, minTime = 5, maxTime = 20, isFelony = true,
+    fine = function() return (math.random(100, 140)) end
+  },
+  ['atm'] =  {
+    title = "ATM Burglary",
+    weight = 32, minTime = 5, maxTime = 15, isFelony = false,
+    fine = function() return (math.random(600, 2000)) end
+  },
+  ['unpaid'] = {
+    title = "Unpaid Ticket",
+    weight = 50, minTime = 1, maxTime = 10, isFelony = true,
+    fine = function() return (math.random(100, 500)) end
+  },
+  ['brandish'] = {
+    title = "Firearm Brandished",
+    weight = 5, minTime = 2, maxTime = 5, isFelony = false,
+    fine = function() return (math.random(200, 1000)) end
+  },
+  ['brandish-npc'] = {
+    title = "Firearm Brandished",
+    weight = 5, minTime = 1, maxTime = 2, isFelony = false,
+    fine = function() return (math.random(100, 500)) end
+  },
+  ['brandish-leo'] = {
+    title = "Firearm Brandished on a LEO",
+    weight = 50, minTime = 5, maxTime = 10, isFelony = true,
+    fine = function() return (math.random(500, 1200)) end
+  },
+  ['prisonbreak'] = {
+    title = "Prison Break",
+    weight = 400, minTime = 20, maxTime = 30, isFelony = true,
+    fine = function() return (math.random(12000, 20000)) end
+  },
+  ['jailbreak'] = {
+    title = "Jailbreak",
+    weight = 120, minTime = 5, maxTime = 10, isFelony = true,
+    fine = function() return (math.random(5000, 8000)) end
+  },
+  ['traffic_drug'] = {
+    title = "Drug Trafficking",
+    weight = 30, minTime = 12, maxTime = 20, isFelony = true,
+    fine = function() return (math.random(100, 500)) end
+  },
+  ['traffic_guns'] = {
+    title = "Weapons Trafficking",
+    weight = 30, minTime = 12, maxTime = 20, isFelony = true,
+    fine = function() return (math.random(100, 500)) end
+  },
+  ['traffic_chop'] = {
+    title = "Possession of Chopshop Parts",
+    weight = 30, minTime = 5, maxTime = 12, isFelony = true,
+    fine = function() return (math.random(100, 500)) end
+  },
+  ['trafficking'] = {
+    title = "Human Trafficking",
+    weight = 180, minTime = 15, maxTime = 40, isFelony = true,
+    fine = function() return (math.random(6000, 9000)) end
+  },
+  ['kidnapping-npc'] = {
+    title = "Kidnapping",
+    weight = 90, minTime = 5, maxTime = 10, isFelony = true,
+    fine = function() return (math.random(100, 500)) end
+  },
+  ['auto-export'] = {
+    title = "Illegal Vehicle Sale",
+    weight = 60, minTime = 5, maxTime = 10, isFelony = true,
+    fine = function() return (math.random(2400, 8000)) end
+  },
+  ['jailed'] = {
+    title = "Jailed/Clear",
+    weight = 0, minTime = 0, maxTime = 0, isFelony = true,
+    fine = 0
+  },
+}
+
+--- EXPORT: GetCrimeName()
+-- Returns the proper name of the given crime.
+-- @param crime The string of the title of the crime (carjack, murder, etc)
+-- @return The name of the crime (always string, 'crime' if not found)
+function GetCrimeName(crime)
+  if not crime               then  return  "crime"  end
+  if not crimes[crime]       then  return  "crime"  end
+  if not crimes[crime].title then  return  "crime"  end
+  return crimes[crime].title
+end
+
+
+--- EXPORT: GetCrimeTime()
+-- Returns the generated time for the given crime
+-- @param crime The string of the title of the crime (carjack, murder, etc)
+-- @return The time (in minutes) to serve. If not found, returns 0 minutes
+function GetCrimeTime(crime)
+  if not crime         then return 0 end
+  if not crimes[crime] then return 0 end
+  local c = crimes[crime]
+  if not c.minTime then c.minTime =  5 end
+  if not c.maxTime then c.maxTime = 10 end
+  local cTime = math.random(c.minTime, c.maxTime)
+  return cTime
+end
+
+
+--- EXPORT: GetCrimeFine()
+-- Returns the generated fine for the crime
+-- @param crime The string of the title of the crime (carjack, murder, etc)
+-- @return The time (in minutes) to serve. If not found, returns 50 dollars
+function GetCrimeFine(crime)
+  if not crime              then  return 0 end
+  if not crimes[crime]      then  return 0 end
+  if not crimes[crime].fine then  return 0 end
+  return (crimes[crime].fine())
+end
+
+
+--- EXPORT: IsCrimeFelony()
+-- Gets whether the given crime is a felony
+-- @param crime The string of the title of the crime (carjack, murder, etc)
+-- @return The time (in minutes) to serve. If not found, returns 50 dollars
+function IsCrimeFelony(crime)
+  if not crime                  then  return false  end
+  if not crimes[crime]          then  return false  end
+  if not crimes[crime].isFelony then  return false  end
+  return (crimes[crime].isFelony)
+end
+
+
+--- EXPORT: GetCrimeWeight()
+-- Gets the severity of a crime
+-- @param crime The string of the title of the crime (carjack, murder, etc)
+-- @return The severity weight, where 1 is least severe
+function GetCrimeWeight(crime)
+  if not crime                 then  return 1 end
+  if not crimes[crime]         then  return 1 end
+  if not crimes[crime].weight  then  return 1 end
+  return (crimes[crime].weight)
+end
+
+
+--- EXPORT: DoesCrimeExist()
+-- Checks if the given crime index exists in the table
+-- @param crime The string to check for
+-- @return True if the crime exists, false if it does not
+function DoesCrimeExist(crime)
+  if crimes[crime] then
+    if crimes[crime].title then
+      return true
+    else
+      cprint("^1Crime '"..tostring(crime).."' did not exist in sh_wanted.lua")
+      return false
+    end
+  else
+    cprint("^1Crime '"..tostring(crime).."' did not exist in sh_wanted.lua")
+    return false
+  end
+end
