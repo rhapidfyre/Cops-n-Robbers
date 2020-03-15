3d0b1b22021dcaa9ed3ee9f6c9a70350587199c0
rhapidfyre <maharris84@hotmail.com>
2020-03-15 10:04:02 -0700

    Added cash & bank values to radar

--- cnr_cash/nui/ui.js
+++ cnr_cash/nui/ui.js
@@ -1,19 +1,31 @@
 
 $(function()
 {
-  
-  var wnts = $("#wantedstars");
-  
+
+  let monies = $("#monies");
+
   window.addEventListener('message', function(event)
   {
       var item = event.data;
-      
-    // Pressing the ESC key with the menu open closes it 
+
+      if (item.showmoney) { monies.fadeIn(1000); }
+
+      if (item.hidemoney) { monies.fadeOut(200); }
+
+      if (item.cashbalance) {
+        $("#handcash").html(item.cashbalance);
+      }
+
+      if (item.bankbalance) {
+        $("#bankcash").html(item.bankbalance);
+      }
+
+    /* Pressing the ESC key with the menu open closes it
     document.onkeyup = function ( data ) {
       if (data.which == 27) {
-        if (rspn.is(":visible")) {Confirm();}
-        if (cad.is(":visible")) {doExit();}
+
       }
     };
+    */
   });
 });
