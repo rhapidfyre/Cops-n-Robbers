63fe402d01ad57809222f4e3d62a4121db950f54
rhapidfyre <maharris84@hotmail.com>
2020-03-16 09:48:32 -0700

    Show HUD cash only when large-map (Z) is open

--- cnr_cash/cl_cash.lua
+++ cnr_cash/cl_cash.lua
@@ -4,13 +4,18 @@ RegisterNetEvent('cnr:atm_user')
 local myCash, myBank = 0, 0
 
 
+AddEventHandler('cnr:bigmap', function(mapOpened)
+  if mapOpened then SendNUIMessage({showmoney = true})
+  else              SendNUIMessage({hidemoney = true})
+  end
+end)
+
 -- addComma()
 -- Adds a comma every 3 digits to format the cash value
 -- @param
 local function addComma(str)
 	return #str % 3 == 0 and str:reverse():gsub("(%d%d%d)","%1,"):reverse():sub(2) or str:reverse():gsub("(%d%d%d)", "%1,"):reverse()
 end
-
 
 --- EXPORT: CashOnHand()
 -- Returns the player's current wallet value
