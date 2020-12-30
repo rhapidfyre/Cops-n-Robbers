768698ab24b5beeb1ee4152473541bc19579788e
rhapidfyre <maharris84@hotmail.com>
2020-12-30 10:12:45 -0800

    death stuff
    death stuff

--- cnrobbers/client/death.lua
+++ cnrobbers/client/death.lua
@@ -1,5 +1,4 @@
 
-RegisterNetEvent('cnr:death_insurance')
 RegisterNetEvent('cnr:police_imprison')
 RegisterNetEvent('cnr:prison_release')
 RegisterNetEvent('cnr:prison_rejail')
@@ -9,66 +8,12 @@ local locksound = false
 local hNear     = 0
 local hasInsurance = false
 local passiveMode = false
+local notified = false
 
-local hospitals = {
-  [1] = {
-    coords      = vector3(-497.55, -335.841, 34.51),
-    deathcam    = vector3(-465.31, -373.534, 39.05),
-    insure      = vector3(-493.62, -325.66, 33.40),
-    pedHeading  = 266.0, camHeading = 20.0,
-    jailHospital = 0,
-    title       = "Mount Zonah Medical Center"
-  },
-  [2] = {
-    coords      = vector3(295.424, -1447.42, 29.97),
-    deathcam    = vector3(273.746, -1395.03, 34.51),
-    insure      = vector3(306.268, -1433.35, 28.97),
-    pedHeading  = 320.0, camHeading = 190.0,
-    jailHospital = 0,
-    title       = "UC Los Santos"
-  },
-  [3] = {
-    coords      = vector3(-247.909, 6332.7, 32.4262),
-    deathcam    = vector3(-217.081, 6317.74, 35.891),
-    insure      = vector3(-243.758, 6325.6, 31.32),
-    pedHeading  = 222.0, camHeading = 86.0,
-    jailHospital = 0,
-    title       = "Paleto Bay Medical Center"
-  },
-  [4] = {
-    coords      = vector3(1839.26, 3672.99, 34.276),
-    deathcam    = vector3(1841.92, 3646.32, 37.151),
-    insure      = vector3(1835.78, 3671.58, 34.28),
-    pedHeading  = 210.0, camHeading = 0.0,
-    jailHospital = 0,
-    title       = "Sandy Shores Care Facility"
-  },
-  [5] = {
-    coords      = vector3(359.99, -585.134, 28.82),
-    deathcam    = vector3(391.35, -571.819, 31.5),
-    insure      = vector3(361.28, -580.45, 27.73),
-    pedHeading  = 230.0, camHeading = 125.0,
-    jailHospital = 0,
-    title       = "Pillbox Medical Center"
-  },
-  [6] = {
-    coords      = vector3(1662.25, 2592.99, 45.56),
-    deathcam    = vector3(1642.71, 2605.39, 48.68),
-    pedHeading  = 82.0, camHeading = 220.0,
-    jailHospital = 2,
-    title       = "Bolingbroke Medical Center"
-  },
-  [7] = {
-    coords      = vector3(460.97, -1001.05, 24.91),
-    deathcam    = vector3(465.12, -1002.69, 25.91),
-    pedHeading  = 272.0, camHeading = 66.0,
-    jailHospital = 1,
-    title       = "Mission Row Medical Bay"
-  },
-}
 
--- Add Hospital Blips
 Citizen.CreateThread(function()
+
+  -- Add hospital blips
   for _,v in pairs(hospitals) do
     if v.jailHospital < 1 then
       local blip = AddBlipForCoord(v.coords)
@@ -82,40 +27,40 @@ Citizen.CreateThread(function()
       EndTextCommandSetBlipName(blip)
     end
   end
+  
+  -- Track hospital information (if one is nearby)
   while true do
     if hNear > 0 then
-      local iPos = hospitals[hNear].insure
-      DrawMarker(1, iPos.x, iPos.y, iPos.z,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        1.25, 1.25, 0.65, 255, 255, 255, 120,
-        false, false, 0, false
-      )
-      DrawMarker(29, iPos.x, iPos.y, iPos.z + 1.3,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        1.25, 1.25, 0.65, 255, 65, 65, 255,
-        false, false, 0, true
-      )
-      if #(GetEntityCoords(PlayerPedId()) - iPos) < 3.25 then
-        if not hasInsurance then
+    
+      -- Can only buy insurance at non-jail hospitals
+      if not hospitals[hNear].jailHospital then
+        local iPos = hospitals[hNear].insure
+        DrawMarker(1, iPos.x, iPos.y, iPos.z,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+          2.25, 2.25, 0.65, 255, 255, 255, 120,
+          false, false, 0, false
+        )
+        DrawMarker(29, iPos.x, iPos.y, iPos.z + 1.3,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+          1.2, 1.2, 1.2, 255, 65, 65, 255,
+          false, false, 0, true
+        )
+        if #(GetEntityCoords(PlayerPedId()) - iPos) < 2.25 then
           ClearPrints()
           SetTextEntry_2("STRING")
           AddTextComponentString("[~g~E~w~]: Buy Insurance ($25,000)")
-          DrawSubtitleTimed(count, 1)
+          DrawSubtitleTimed(1, 1)
           if IsControlJustPressed(0, 38) then
             TriggerServerEvent('cnr:death_buy_insurance')
-            Citizen.Wait(3000)
+            Citizen.Wait(1000)
           end
-        else
-          ClearPrints()
-          SetTextEntry_2("STRING")
-          AddTextComponentString("~r~You already have Health Insurance")
-          DrawSubtitleTimed(count, 1)
         end
       end
-    else Citizen.Wait(3000)
+      
     end
-    Citizen.Wait(0)
+    Citizen.Wait(1)
   end
+  
 end)
 
 
@@ -139,13 +84,11 @@ Citizen.CreateThread(function()
 end)
 
 
-local notified = false
 local function DeathNotification()
   if not notified then
     notified = true
     local cause  = GetPedCauseOfDeath(PlayerPedId())
     local killer = GetPedSourceOfDeath(PlayerPedId())
-    print(cause, killer)
     if DoesEntityExist(killer) then
       if IsEntityAPed(killer) then
         if IsPedAPlayer(killer) then
@@ -194,53 +137,61 @@ local function DeathNotification()
         TriggerServerEvent('cnr:death_check', nil)
       end
     end
-    Citizen.Wait(2000)
     TriggerEvent('cnr:player_died')
     TriggerServerEvent('cnr:player_death')
-    Citizen.Wait(3000)
+    
+    -- Prevent repeat checks/messages per death
+    Citizen.Wait(5000)
     notified = false
+    
   end
 end
 
+
 Citizen.CreateThread(function()
-   while true do
-       Citizen.Wait(0)
-       if IsPlayerDead(PlayerId()) then
-         Citizen.CreateThread(RevivePlayer)
-         Citizen.CreateThread(DeathNotification)
-         StartScreenEffect("DeathFailOut", 0, 0)
-         if not locksound then
-           PlaySoundFrontend(-1, "Bed", "WastedSounds", 1)
-           locksound = true
-         end
-         ShakeGameplayCam("DEATH_FAIL_IN_EFFECT_SHAKE", 1.0)
-
-         local scaleform = RequestScaleformMovie("MP_BIG_MESSAGE_FREEMODE")
-
-
-         if HasScaleformMovieLoaded(scaleform) then
-           Citizen.Wait(0)
-
-           PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-           BeginTextComponent("STRING")
-           AddTextComponentString("~r~wasted")
-           EndTextComponent()
-           PopScaleformMovieFunctionVoid()
-
-           Citizen.Wait(500)
-
-           PlaySoundFrontend(-1, "TextHit", "WastedSounds", 1)
-           while IsPlayerDead(PlayerId()) do
-             DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
-             HideHudAndRadarThisFrame(true)
-             Citizen.Wait(0)
-           end
-           StopScreenEffect("DeathFailOut")
-           locksound = false
-         end
-
-       end
+  while not CNR do Wait(100) end
+  while true do
+    Citizen.Wait(0)
+    if CNR.loaded then
+      if not CNR.dead then
+        if IsPlayerDead(PlayerId()) or IsPedDeadOrDying(PlayerPedId()) then
+          CNR.dead = true
+        end
+        Citizen.CreateThread(RevivePlayer)
+        Citizen.CreateThread(DeathNotification)
+        StartScreenEffect("DeathFailOut", 0, 0)
+        if not locksound then
+          PlaySoundFrontend(-1, "Bed", "WastedSounds", 1)
+          locksound = true
+        end
+        ShakeGameplayCam("DEATH_FAIL_IN_EFFECT_SHAKE", 1.0)
+  
+        local scaleform = RequestScaleformMovie("MP_BIG_MESSAGE_FREEMODE")
+  
+  
+        if HasScaleformMovieLoaded(scaleform) then
+          Citizen.Wait(0)
+  
+          PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
+          BeginTextComponent("STRING")
+          AddTextComponentString("~r~wasted")
+          EndTextComponent()
+          PopScaleformMovieFunctionVoid()
+  
+          Citizen.Wait(500)
+  
+          PlaySoundFrontend(-1, "TextHit", "WastedSounds", 1)
+          while IsPlayerDead(PlayerId()) do
+            DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
+            HideHudAndRadarThisFrame(true)
+            Citizen.Wait(0)
+          end
+          StopScreenEffect("DeathFailOut")
+          locksound = false
+        end
+      end
     end
+  end
 end)
 
 local doingRevive = false
@@ -262,7 +213,8 @@ function RevivePlayer()
 
     -- Return closest hospital OR jail/prison hospital
     for k,v in pairs (hospitals) do
-      if v.jailHospital == isPrison then
+      if Imprisoned() then
+      if v.jailHospital then
         local dist = #(myPos - v.coords)
         if dist < cDist then nearest = k; cDist = dist end
       end
@@ -346,43 +298,6 @@ function RevivePlayer()
   doingRevive = false
 end
 
-
-
-AddEventHandler('cnr:death_insurance', function(insuranceValue)
-
-  local wl = exports['cnr_wanted']:WantedLevel()
-  if not insuranceValue then insuranceValue = 0 end
-  if insuranceValue > 0 then
-
-    if insuranceValue == 2 then
-      TriggerEvent('chat:addMessage', {templateId = 'sysMsg', args = {
-        "Since you died in police custody, your personal items have been saved."
-      }})
-
-    else
-      TriggerEvent('chat:addMessage', {templateId = 'sysMsg', args = {
-        "Your insurance prevented you from losing your personal items!\n"..
-        "You will have to renew your health insurance before dying again."
-      }})
-
-    end
-
-  else
-
-    RemoveAllPedWeapons(PlayerPedId(), true)
-    TriggerEvent('chat:addMessage', {templateId = 'sysMsg', args = {
-      "You died without health insurance! Welcome to your new life!"
-    }})
-
-  end
-  hasInsurance = false
-end)
-
-
-RegisterNetEvent('cnr:death_has_insurance', function()
-  print("DEBUG - You have insurance!")
-  hasInsurance = true
-end)
 
 RegisterNetEvent('cnr:death_notify')
 AddEventHandler('cnr:death_notify', function(v, k)
---
+++ cnrobbers/shared/death.lua
@@ -0,0 +1,45 @@
+
+hospitals = {
+  [1] = {
+    coords        = vector3(-497.55, -335.841, 34.51),
+    deathcam      = vector3(-465.31, -373.534, 39.05),
+    insure        = vector3(-493.62, -325.66, 33.40),
+    pedHeading    = 266.0, camHeading = 20.0,
+    title         = "Mount Zonah Medical Center"
+  },
+  [2] = {
+    coords        = vector3(295.424, -1447.42, 29.97),
+    deathcam      = vector3(273.746, -1395.03, 34.51),
+    insure        = vector3(306.268, -1433.35, 28.97),
+    pedHeading    = 320.0, camHeading = 190.0,
+    title         = "UC Los Santos"
+  },
+  [3] = {
+    coords        = vector3(-247.909, 6332.7, 32.4262),
+    deathcam      = vector3(-217.081, 6317.74, 35.891),
+    insure        = vector3(-243.758, 6325.6, 31.32),
+    pedHeading    = 222.0, camHeading = 86.0,
+    title         = "Paleto Bay Medical Center"
+  },
+  [4] = {
+    coords        = vector3(1839.26, 3672.99, 34.276),
+    deathcam      = vector3(1841.92, 3646.32, 37.151),
+    insure        = vector3(1835.78, 3671.58, 34.28),
+    pedHeading    = 210.0, camHeading = 0.0,
+    title         = "Sandy Shores Care Facility"
+  },
+  [5] = {
+    coords        = vector3(359.99, -585.134, 28.82),
+    deathcam      = vector3(391.35, -571.819, 31.5),
+    insure        = vector3(361.28, -580.45, 27.73),
+    pedHeading    = 230.0, camHeading = 125.0,
+    title         = "Pillbox Medical Center"
+  },
+  [6] = {
+    coords        = vector3(1662.25, 2592.99, 45.56),
+    deathcam      = vector3(1642.71, 2605.39, 48.68),
+    pedHeading    = 82.0, camHeading = 220.0,
+    jailHospital  = true,
+    title         = "Bolingbroke Medical Center"
+  }
+}
